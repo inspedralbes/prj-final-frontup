b81696dbb42206c1ac8f5f18c49e7922a51ea41a
AyoubBoudhafr <a23mohbounij@inspedralbes.cat>
2025-01-23 10:14:39 +0100

    creado componente de prueba

---
+++ front/pages/prueba.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <MiNuevoComponente />
+    </div>
+  </template>
+  
+  <script>
+  import MiNuevoComponente from '~/components/MiNuevoComponente.vue'
+  
+  export default {
+    components: {
+      prueba,
+    },
+    layout: 'default',
+  }
+  </script>
+  
+  <style scoped>
+  div {
+    padding: 1rem;
+  }
+  </style>
+  
