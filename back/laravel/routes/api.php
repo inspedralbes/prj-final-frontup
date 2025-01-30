d03803ada87bc9900fd56ddd37b47fb32cb9ee2c
Gerard Arias <a23gerarimar@inspedralbes.cat>
2025-01-30 10:22:17 +0100

    sanctum

--- back/laravel/routes/api.php
+++ back/laravel/routes/api.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\UserController;
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', [UserController::class, 'getUser']);
+    Route::post('/user/avatar', [UserController::class, 'updateAvatar']);
 });
 Route::post('/projects', [ProjectController::class, 'store']);      
 Route::get('/projects', [ProjectController::class, 'index']);       
@@ -14,8 +16,6 @@ Route::get('/projects/{id}', [ProjectController::class, 'show']);
 Route::put('/projects/{id}', [ProjectController::class, 'update']); 
 Route::delete('/projects/{id}', [ProjectController::class, 'destroy']);
 
-Route::get('/user', [UserController::class, 'getUser']);
-Route::post('/user/avatar', [UserController::class, 'updateAvatar']);
 
 Route::post('/register', [AuthenticatorController::class, 'register']);
 
