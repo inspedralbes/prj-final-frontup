d25559445d3e25e4f2f7bf466e3e9e0ba90c333c
AyoubBoudhafr <a23mohbounij@inspedralbes.cat>
2025-04-02 10:03:11 +0200

    likes funcionan en back con postman

--- back/laravel/routes/api.php
+++ back/laravel/routes/api.php
@@ -6,24 +6,37 @@ use App\Http\Controllers\AuthenticatorController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\PreguntaController;
+use App\Http\Controllers\LikesController;
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', [UserController::class, 'getUser']);
     Route::post('/user/avatar', [UserController::class, 'updateAvatar']);
+    //proyectos del usuario solo
     Route::get('/projects', [ProjectController::class, 'index']);
-});
 
-Route::get('/projects/all', [ProjectController::class, 'indexAll']);//Front 
+    //dar like
+    Route::post('/likes', [LikesController::class, 'store']);
+    //borrar like
+    Route::delete('/likes/{projectId}', [LikesController::class, 'destroy']);
+    //chequea si le has dado like a una publicacion
+    Route::get('/likes/check/{projectId}', [LikesController::class, 'checkLike']);
+    //todos los likes que has dado
+    Route::get('/likes/user', [LikesController::class, 'userLikes']);
+});
+//contador de likes para un proyecto
+Route::get('/likes/count/{projectId}', [LikesController::class, 'likeCount']);
+//indexa proyectos de todos los usuarios
+Route::get('/projects/all', [ProjectController::class, 'indexAll']); 
 Route::post('/projects', [ProjectController::class, 'store']);      
 Route::get('/projects/{id}', [ProjectController::class, 'show']);   
 Route::put('/projects/{id}', [ProjectController::class, 'update']); 
 Route::delete('/projects/{id}', [ProjectController::class, 'destroy']);
-Route::get('/projectsAllPaginado', [ProjectController::class, 'index']);//Front 
+Route::get('/projectsAllPaginado', [ProjectController::class, 'index']);
 
 
 Route::post('/register', [AuthenticatorController::class, 'register']);
 
 Route::post('/login', [AuthenticatorController::class, 'authenticate']);    
-
 
 Route::get('/preguntas/{language}/{id}', [PreguntaController::class, 'getPreguntas']);
 
