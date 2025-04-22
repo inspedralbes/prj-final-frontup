c796b5c4f7db15dcf8ba1eab11ec8f965244dff2
Gerard Arias <a23gerarimar@inspedralbes.cat>
2025-04-22 12:53:12 +0200

    a

--- front/pages/lliure/[id].vue
+++ front/pages/lliure/[id].vue
@@ -121,6 +121,15 @@ import "codemirror/mode/htmlmixed/htmlmixed";
 import "codemirror/mode/css/css";
 import "codemirror/mode/javascript/javascript";
 
+import "codemirror/addon/hint/show-hint";
+import "codemirror/addon/hint/show-hint.css";
+import "codemirror/addon/hint/html-hint";
+import "codemirror/addon/hint/anyword-hint";
+import "codemirror/addon/edit/closetag";
+import "codemirror/addon/edit/matchtags";
+import "codemirror/addon/hint/javascript-hint";
+import "codemirror/addon/edit/closebrackets";
+
 import useCommunicationManager from "@/stores/comunicationManager";
 import { useAppStore, useIdProyectoActualStore } from "@/stores/app";
 
